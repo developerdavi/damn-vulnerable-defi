b00646be194103a50a8e2becd33705f726d703b5
Davi Rodrigues <developer.davi@gmail.com>
2022-10-17 13:35:00 -0300

    style: add linting

--- contracts/WETH9.sol
+++ contracts/WETH9.sol
@@ -1,8 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED
+
 // Taken from address 0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2 at mainnet
 
 /**
  *Submitted for verification at Etherscan.io on 2017-12-12
-*/
+ */
 
 // Copyright (C) 2015, 2016, 2017 Dapphub
 
@@ -22,68 +24,68 @@
 pragma solidity ^0.7.0;
 
 contract WETH9 {
-    string public name     = "Wrapped Ether";
-    string public symbol   = "WETH";
-    uint8  public decimals = 18;
+  string public name = 'Wrapped Ether';
+  string public symbol = 'WETH';
+  uint8 public decimals = 18;
 
-    event  Approval(address indexed src, address indexed guy, uint wad);
-    event  Transfer(address indexed src, address indexed dst, uint wad);
-    event  Deposit(address indexed dst, uint wad);
-    event  Withdrawal(address indexed src, uint wad);
+  event Approval(address indexed src, address indexed guy, uint256 wad);
+  event Transfer(address indexed src, address indexed dst, uint256 wad);
+  event Deposit(address indexed dst, uint256 wad);
+  event Withdrawal(address indexed src, uint256 wad);
 
-    mapping (address => uint)                       public  balanceOf;
-    mapping (address => mapping (address => uint))  public  allowance;
+  mapping(address => uint256) public balanceOf;
+  mapping(address => mapping(address => uint256)) public allowance;
 
-    receive() external payable {
-        deposit();
+  receive() external payable {
+    deposit();
+  }
+
+  function deposit() public payable {
+    balanceOf[msg.sender] += msg.value;
+    emit Deposit(msg.sender, msg.value);
+  }
+
+  function withdraw(uint256 wad) public {
+    require(balanceOf[msg.sender] >= wad);
+    balanceOf[msg.sender] -= wad;
+    msg.sender.transfer(wad);
+    emit Withdrawal(msg.sender, wad);
+  }
+
+  function totalSupply() public view returns (uint256) {
+    return address(this).balance;
+  }
+
+  function approve(address guy, uint256 wad) public returns (bool) {
+    allowance[msg.sender][guy] = wad;
+    emit Approval(msg.sender, guy, wad);
+    return true;
+  }
+
+  function transfer(address dst, uint256 wad) public returns (bool) {
+    return transferFrom(msg.sender, dst, wad);
+  }
+
+  function transferFrom(
+    address src,
+    address dst,
+    uint256 wad
+  ) public returns (bool) {
+    require(balanceOf[src] >= wad);
+
+    if (src != msg.sender && allowance[src][msg.sender] != uint256(-1)) {
+      require(allowance[src][msg.sender] >= wad);
+      allowance[src][msg.sender] -= wad;
     }
 
-    function deposit() public payable {
-        balanceOf[msg.sender] += msg.value;
-        emit Deposit(msg.sender, msg.value);
-    }
-    
-    function withdraw(uint wad) public {
-        require(balanceOf[msg.sender] >= wad);
-        balanceOf[msg.sender] -= wad;
-        msg.sender.transfer(wad);
-        emit Withdrawal(msg.sender, wad);
-    }
+    balanceOf[src] -= wad;
+    balanceOf[dst] += wad;
 
-    function totalSupply() public view returns (uint) {
-        return address(this).balance;
-    }
+    emit Transfer(src, dst, wad);
 
-    function approve(address guy, uint wad) public returns (bool) {
-        allowance[msg.sender][guy] = wad;
-        emit Approval(msg.sender, guy, wad);
-        return true;
-    }
-
-    function transfer(address dst, uint wad) public returns (bool) {
-        return transferFrom(msg.sender, dst, wad);
-    }
-
-    function transferFrom(address src, address dst, uint wad)
-        public
-        returns (bool)
-    {
-        require(balanceOf[src] >= wad);
-
-        if (src != msg.sender && allowance[src][msg.sender] != uint(-1)) {
-            require(allowance[src][msg.sender] >= wad);
-            allowance[src][msg.sender] -= wad;
-        }
-
-        balanceOf[src] -= wad;
-        balanceOf[dst] += wad;
-
-        emit Transfer(src, dst, wad);
-
-        return true;
-    }
+    return true;
+  }
 }
-
 
 /*
                     GNU GENERAL PUBLIC LICENSE
